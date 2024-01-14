3eadd8ed78e9cde9cb0fe1f740036a7f9f4c7c4b
sherif Abdelrhman <sellkady038@gmail.com>
2024-01-14 03:24:45 +0200

    Second Commit

---
+++ app/src/main/java/com/route/islamyapp_task/Constants.kt
@@ -0,0 +1,7 @@
+package com.route.islamyapp_task
+
+object Constants {
+    val SOBHANAALLA :String= "سبحان الله"
+    val ELHAMDLLALLA :String= "الحمد لله"
+    val ALLAHAKBER :String= "الله اكبر"
+}
--- app/src/main/java/com/route/islamyapp_task/tasbehFragment.kt
+++ app/src/main/java/com/route/islamyapp_task/tasbehFragment.kt
@@ -5,15 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.route.islamyapp_task.databinding.FragmentTasbehBinding
+import kotlin.properties.Delegates
 
 
 class tasbehFragment : Fragment() {
+    var displayCounter by Delegates.notNull<Int>()
+    lateinit var binding : FragmentTasbehBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasbeh, container, false)
+        binding= FragmentTasbehBinding.inflate(inflater,container,false)
+        return binding.root
     }
+
+//    var displayCounter by Delegates.notNull<Int>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var counter = 1
+        binding.numbersBtn.setOnClickListener {
+             displayCounter = counter++
+            binding.numbersBtn.text=displayCounter.toString()
+            if(displayCounter==1){
+                binding.tsbehBtn.text=Constants.SOBHANAALLA
+            }
+            if(displayCounter==33){
+                binding.tsbehBtn.text=Constants.ELHAMDLLALLA
+            }
+            if(displayCounter==66){
+                binding.tsbehBtn.text=Constants.ALLAHAKBER
+            }
+            if(displayCounter==100){
+                binding.tsbehBtn.text=Constants.SOBHANAALLA
+            }
+//            if(displayCounter==100) {
+//                displayCounter=1
+//            }
+        }
+
+        }
+
 }
